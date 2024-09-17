4444f2f5b8d05b4925fcba2f072ee2590b9781ef
lzk <3089830091@qq.com>
2024-09-18 01:55:10 +0800

    如果没有url跳转到addUrl

--- src/router/permission.ts
+++ src/router/permission.ts
@@ -3,6 +3,7 @@ import { NavigationGuardNext, RouteLocationNormalized } from 'vue-router'
 import getPageTitle from '../utils/get_page_title'
 import { devLog } from '../utils/devLog'
 import {useRoutesStore}from '../store/useRoutesStore'
+import { useUrlStore } from '../store/useUrlStore'
 //白名单
 // const whiteList = ['/', '/forgot']
 devLog("addroute sucsess")
@@ -11,13 +12,31 @@ devLog("addroute sucsess")
 router.beforeEach(async (to: RouteLocationNormalized, from: RouteLocationNormalized, next: NavigationGuardNext) => {
   devLog("before each")
   let routesStore = useRoutesStore()
+  const urlStore = useUrlStore()
   //设置页面标题
   routesStore.generateRoutes()
   document.title = getPageTitle(to.meta.title as string)
-  if (!to.path || to.path === '/') {
-    next({ path: '/index' });
-  } else {
-    next();
+  //if()
+  // if (!to.path || to.path === '/') {
+  //   next({ path: '/index' });
+  // } else {
+  //   next();
+  // }
+
+  if (urlStore.UrlList.length > 0) {
+    // next({ path: '/index' });
+    if (!to.path || to.path === '/') {
+      next({ path: '/index' });
+    } else {
+      next();
+    }
+  }
+  else {
+    if (to.path !== '/addurl') {
+      next({ path: '/addurl' })
+    } else {
+      next()
+    }
   }
 })
 
